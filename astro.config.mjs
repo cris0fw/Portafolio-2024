76ec99716e5a8ac27f5c45df15c16d9d5f37a561
cris0fw <crisludue5@gmail.com>
2024-10-07 19:20:53 -0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: "https://cris0fw.github.io",
+  base: "portafolio-2024",
 });
